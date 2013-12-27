9a09dbef8146cae574ed902b627d5a3014670711
Tony Kovanen <tonykovanen@hotmail.com>
2013-12-27 14:22:51 +0200

    Making changes

--- regression.hs
+++ regression.hs
@@ -3,21 +3,27 @@ module MachineLearning.Regression
 , Dataset(..)
 , ParamFindStrategy(..)
 , findParameters
-, run
+, fromList
+--, run
 ) where
 
-import Numeric.LinearAlgebra
+import qualified Numeric.LinearAlgebra as L
 
 -- TODO: sort of unnecessary to handle all the stuff as lists... why not just
 -- have then as matrices to begin with?
 
-newtype Parameters = Parameters [Double] deriving (Show, Eq)
+newtype Parameters = Parameters (L.Matrix Double) deriving (Show, Eq)
+
+fromList :: [[Double]] -> L.Matrix Double
+fromList xs = (samples L.>< features) $ concat xs
+  where samples = length xs
+        features = length $ head xs
 
 -- For now only support for double values
 -- For now implementation shall stay as a list, but all rows should be of equal
 -- length
-type X = [[Double]]
-type Y = [Double]
+type X = L.Matrix Double
+type Y = L.Matrix Double
 type Results = [Double]
 data Dataset = TrainingSet X Y | TestSet X
 
@@ -25,22 +31,18 @@ data ParamFindStrategy = NormalEquation | GradientDescent
 
 findParameters :: Dataset -> ParamFindStrategy -> Parameters
 findParameters (TestSet x) _ = error "You can't find parameters with label-less data"
-findParameters (TrainingSet x y) NormalEquation = Parameters $ concat (toLists $ multiply (pinv $ multiply (ctrans xMat) xMat) (multiply (ctrans xMat) yMat))
-  where xMat = (samples >< features) $ concat x
-        yMat = (samples >< 1) y
-        samples = length x
-        features = length (head x)
-findParameters d GradientDescent = Parameters [] -- TODO
+findParameters (TrainingSet x y) NormalEquation = Parameters $ L.multiply (L.pinv $ L.multiply (L.ctrans x) x) (L.multiply (L.ctrans x) y)
+findParameters d GradientDescent = Parameters $ fromList [[1,2,3]] -- TODO
 
-run :: Dataset -> Parameters -> Results
-run (TrainingSet x y) _ = error "You should not run experiments with training data" -- Actually maybe you should be able
-run (TestSet ts) p = map (runIndividual p) ts
-
--- Running the experiment for an individual datapoint
-runIndividual :: Parameters -> [Double] -> Double
-runIndividual (Parameters p) row = sum $ zipWith (*) row p
+-- Deprecated... find out how to map with row of matrix
+--run :: Dataset -> Parameters -> Results
+--run (TrainingSet x y) _ = error "You should not run experiments with training data" -- Actually maybe you should be able
+--run (TestSet ts) p = map (runIndividual p) ts
+--
+---- Running the experiment for an individual datapoint
+--runIndividual :: Parameters -> [Double] -> Double
+--runIndividual (Parameters p) row = undefined
 
 -- Just testing stuff out
-main = do putStrLn (show $ Parameters [1,2,3,4])
-          putStrLn (show $ findParameters (TrainingSet [[1, 2, 3], [2, 2, 2], [2, 1, 2]] [1, 2, 2]) NormalEquation)
-          putStrLn (show $ run (TestSet [[1, 2, 3], [1, 2, 3]]) (Parameters [1, 2, 3]))
+main = do putStrLn (show . Parameters $ fromList [[1, 2, 3, 4]])
+          putStrLn (show $ findParameters (TrainingSet (fromList [[1,2,3],[1,2,3],[2,2,3]]) (fromList [[1],[2],[3]])) NormalEquation)
